616422c0bf57afad0c17fcee5aba76d0a9324eb7
Renan Santos Mendes <renansantosmendes@gmail.com>
2023-06-06 22:26:28 -0300

    Update train.py

--- train.py
+++ train.py
@@ -9,14 +9,12 @@ from keras.layers import Dense, Activation, InputLayer, Dropout
 from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
 from keras.regularizers import l1, l2
-
+import mlflow
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
-"""# Definindo funções adicionais"""
 
 import os
 import random
@@ -29,60 +27,65 @@ def reset_seeds():
    np.random.seed(42)
    random.seed(42)
 
-"""# 2 - Fazendo a leitura do dataset e atribuindo às respectivas variáveis"""
 
-data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+def read_data():
+    return pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
 
-"""# Dando uma leve olhada nos dados"""
 
-data.head()
+def prepare_data(data):
+    X=data.drop(["fetal_health"], axis=1)
+    y=data["fetal_health"]
 
-"""# 3 - Preparando o dado antes de iniciar o treino do modelo"""
+    columns_names = list(X.columns)
+    scaler = preprocessing.StandardScaler()
+    X_df = scaler.fit_transform(X)
+    X_df = pd.DataFrame(X_df, columns=columns_names)
 
-X=data.drop(["fetal_health"], axis=1)
-y=data["fetal_health"]
+    X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.3, random_state=42)
 
-columns_names = list(X.columns)
-scaler = preprocessing.StandardScaler()
-X_df = scaler.fit_transform(X)
-X_df = pd.DataFrame(X_df, columns=columns_names)
+    y_train = y_train -1
+    y_test = y_test - 1
+    return X_train, X_test, y_train, y_test
 
-X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.3, random_state=42)
 
-y_train = y_train -1
-y_test = y_test - 1
+def create_model(input_shape):
+    reset_seeds()
+    model = Sequential()
+    model.add(InputLayer(input_shape=(input_shape, )))
+    model.add(Dense(10, activation='relu' ))
+    model.add(Dense(10, activation='relu' ))
+    model.add(Dense(3, activation='softmax'))
+    return model
 
-"""# 4 - Criando o modelo e adicionando as camadas"""
 
-reset_seeds()
-model = Sequential()
-model.add(InputLayer(input_shape=(X_train.shape[1], )))
-model.add(Dense(10, activation='relu' ))
-model.add(Dense(10, activation='relu' ))
-model.add(Dense(3, activation='softmax' ))
+def compile_model(model):
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
 
-"""# 5 - Compilando o modelo
 
-"""
+def config_mlflow():
+    MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/mlops_puc_230523.mlflow'
+    MLFLOW_TRACKING_USERNAME = 'renansantosmendes'
+    MLFLOW_TRACKING_PASSWORD = 'cc41cc48f8e489dd5b87404dd6f9720944e32e9b'
+    os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.tensorflow.autolog(log_models=True,
+                              log_input_examples=True,
+                              log_model_signatures=True)
 
-import mlflow
 
-MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/mlops_puc_230523.mlflow'
-MLFLOW_TRACKING_USERNAME = 'renansantosmendes'
-MLFLOW_TRACKING_PASSWORD = 'cc41cc48f8e489dd5b87404dd6f9720944e32e9b'
-os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
-os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
+def train_model(model, X_train, y_train):
+    with mlflow.start_run(run_name='experiment_01') as run:
+      model.fit(X_train, y_train, epochs=20, validation_split=0.2, verbose=3)
 
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.tensorflow.autolog(log_models=True, 
-                          log_input_examples=True,
-                          log_model_signatures=True)
 
-"""# 6 - Executando o treino do modelo"""
-
-with mlflow.start_run(run_name='experiment_01') as run:
-  model.fit(X_train, y_train, epochs=20, validation_split=0.2, verbose=3)
+if __name__ == '__main__':
+    reset_seeds()
+    data = read_data()
+    X_train, X_test, y_train, y_test = prepare_data(data)
+    model = create_model(X_train.shape[1])
+    compile_model(model)
+    train_model(model, X_train, y_train)
